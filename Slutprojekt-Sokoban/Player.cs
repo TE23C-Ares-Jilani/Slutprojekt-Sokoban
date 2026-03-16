047371f84507cfe68c7af9f0f9598d93a71cd2f2
Ares Jilani <ares.jilani@elev.ga.ntig>
2026-03-16 13:05:14 +0100

    dfgbsrt

--- Slutprojekt-Sokoban/Player.cs
+++ Slutprojekt-Sokoban/Player.cs
@@ -8,6 +8,8 @@ public Vector2 position;
 public Rectangle rect;
     public void update()
     {
+
+        rect = new(position.X, position.Y, 100, 100);
         if(Raylib.IsKeyDown(KeyboardKey.W))
         {
             rect.X ++;
