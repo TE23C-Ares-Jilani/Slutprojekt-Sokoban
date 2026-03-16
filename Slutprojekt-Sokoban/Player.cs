9c5dcabc6147a43e6b414e00ed561cc95d9a8f47
Ares Jilani <ares.jilani@elev.ga.ntig>
2026-03-16 13:54:00 +0100

    aaaa

--- Slutprojekt-Sokoban/Player.cs
+++ Slutprojekt-Sokoban/Player.cs
@@ -1,18 +1,20 @@
+using System.Dynamic;
 using System.Numerics;
 using System.Runtime.CompilerServices;
 using Raylib_cs;
 
 public class Player
 {
-public Vector2 position;
+public Vector2 position = new(400, 300);
 public Rectangle rect;
     public void update()
     {
+        
 
         rect = new(position.X, position.Y, 100, 100);
         if(Raylib.IsKeyDown(KeyboardKey.W))
         {
-            rect.X ++;
+            position.X ++;
         }
     }
 }
--- Slutprojekt-Sokoban/Program.cs
+++ Slutprojekt-Sokoban/Program.cs
@@ -13,13 +13,21 @@ int[,] grid = new int[10,15];
 
 Player frog = new Player();
 
+
 while (!Raylib.WindowShouldClose())
 {
+    int mouseX = Raylib.GetMouseX();
+    int mouseY = Raylib.GetMouseY();
+
+    frog.position.Y = 400;
+    frog.position.X = 300;
+
     Raylib.ClearBackground(Color.Black);
 
     Raylib.BeginDrawing();
 
     Raylib.DrawRectangleRec(frog.rect, Color.Red);
+    Raylib.DrawRectangle(mouseX,mouseY,100,100,Color.DarkGray);
 
     Raylib.EndDrawing();
 }
