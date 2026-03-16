06040f5f73f792b649d240564b5fcbb8824a65f1
Ares Jilani <ares.jilani@elev.ga.ntig>
2026-03-16 12:15:29 +0100

    i deed it

---
+++ Slutprojekt-Sokoban/Car.cs
@@ -0,0 +1,8 @@
+using System;
+
+namespace Slutprojekt_Sokoban;
+
+public class Car
+{
+
+}
--- Slutprojekt-Sokoban/Program.cs
+++ Slutprojekt-Sokoban/Program.cs
@@ -1,2 +1,21 @@
 ﻿using Raylib_cs;
 
+Raylib.InitWindow(800, 600, "Simple Frogger");
+Raylib.SetTargetFPS(60);
+
+while (!Raylib.WindowShouldClose())
+{
+    Raylib.ClearBackground(Color.Black);
+
+    int mouseX = Raylib.GetMouseX();
+    int mouseY = Raylib.GetMouseY();
+
+    Raylib.BeginDrawing();
+
+    Raylib.DrawCircle(mouseX, mouseY, 20, Color.Red);
+
+    Raylib.EndDrawing();
+}
+
+
+
