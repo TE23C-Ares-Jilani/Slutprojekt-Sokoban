d5b4c6660da11aad804a5333b41c7d410eed144c
Ares Jilani <ares.jilani@elev.ga.ntig>
2026-04-01 15:02:05 +0200

    sdfdfassdaf

--- Slutprojekt-Sokoban/Car.cs
+++ Slutprojekt-Sokoban/Car.cs
@@ -1,4 +1,16 @@
+using System.ComponentModel;
+using System.Drawing;
+using System.Security.Cryptography.X509Certificates;
+
 public class Car
 {
+    public Rectangle obs;
+    static public List<Car> cars = [];
 
+    public Car()
+    {
+        cars.Add(this);
+
+        
+    }
 }
